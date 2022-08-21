12209c362eb054064dade8a3a249c95d56e16a8e
ArbazKhan5117 <arbazkhan5117@gmail.com>
2022-08-21 10:37:51 +0500

    redux tool-kit configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement, incrementByAmount, addUser} from './redux/reducers/reducer'
+//import { addUser } from './redux/reducers/userReducer';
 import './App.css';
 
 function App() {
+  const value = useSelector((state)=> state.counter.value);
+  const allUser = useSelector((state)=> state.counter.names);
+  const [name, setName] = useState();
+  console.log('allUser is ', allUser);
+  const dispatch = useDispatch();
+  const handleAddUser = () =>{
+    const tempArr = [...allUser,name];
+    dispatch(addUser(tempArr));
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <button onClick={()=>dispatch(decrement())}>Decrement</button>
+      <button onClick={()=>dispatch(incrementByAmount(10))}>Increment by 10</button>
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+      <button onClick={handleAddUser}>Add User</button>
+      {allUser?.map((name)=>{
+        return(
+          <h3>{name}</h3>
+        )
+      })}
     </div>
   );
 }
 
 export default App;
+ 
